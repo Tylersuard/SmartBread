738bdc9283c3430267078123a11e6f61190c1709
Tyler <41713505+Tylersuard@users.noreply.github.com>
2021-08-23 20:49:58 -0700

    Create BreadOS.py

---
+++ BreadOS.py
@@ -0,0 +1,15 @@
+for BREAD in BREAD:
+  while BREAD:
+    Bread
+   if BREAD:
+    BREAD
+    
+BREAD BREAD BREAD BREAD
+BREAD BREAD BREAD
+
+BREAD.BREAD(BREAD)
+
+if hacker == true:
+  security == true
+  hacker = false
+  
